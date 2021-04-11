bc7f3126ed6fa64848185ecbdd1bd3bdcca51b23
bastej <siatkasebastian@gmail.com>
2021-04-11 18:50:13 +0200

    Exemplary Carousel using

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Carousel } from "./components/Carousel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Carousel delay={2000}>
+        {100}
+        {"string"}
+        <span>I'm span</span>
+      </Carousel>
     </div>
   );
 }
