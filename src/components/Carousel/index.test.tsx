e68852386f2d7ef3ceff492a4ed7b31b3dbd4732
bastej <siatkasebastian@gmail.com>
2021-12-21 19:25:14 +0100

    Preparing the carousel component testing plan

---
+++ src/components/Carousel/index.test.tsx
@@ -0,0 +1,7 @@
+//  element should be changed after 400ms
+
+// should switch to the first element if the Next button clicked on the last page
+
+// should switch to the last element if the Prev button clicked on the first page
+
+// should reset the delay interval which changing pages automatically if the control button clicked
